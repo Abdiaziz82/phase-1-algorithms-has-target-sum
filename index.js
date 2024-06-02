cb0546f2e35425e1e3e5ad4be5a894c10e79cc8e
Abdiaziz82 <abdiazizhared64@gmail.com>
2024-06-02 10:54:03 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+const array = [3, 8, 12, 4, 11, 7]
+const target = 10
 function hasTargetSum(array, target) {
   // Write your algorithm here
+ for (let i =0; i<array.length; i++){
+  for(let j = i+1; j<array.length; j++)
+ 
+if(array[i] + array[j] === target) {
+  return true
 }
+
+    
+  } return false 
+}
+console.log(hasTargetSum("array","target"))
 
 /* 
   Write the Big O time complexity of your function here
